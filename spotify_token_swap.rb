ecc4b352a37241512cb53407850ea7740105b12a
Pieter Jongsma <pieter.jongsma@gmail.com>
2017-03-17 14:55:55 +0100

    Setup secrets to be fetched from ENV variables

--- spotify_token_swap.rb
+++ spotify_token_swap.rb
@@ -37,9 +37,9 @@ require 'encrypted_strings'
 #
 # Find your app at https://developer.spotify.com/my-applications/#!/applications
 
-CLIENT_ID = "e6695c6d22214e0f832006889566df9c" # TODO: Get it from Spotify
-CLIENT_SECRET = "29eb02041ba646179a1189dccac112c7" # TODO: Get it from Spotify
-CLIENT_CALLBACK_URL = "spotifyiossdkexample://" # TODO: Get it from Spotify
+CLIENT_ID = ENV.fetch("CLIENT_ID") # TODO: Get it from Spotify
+CLIENT_SECRET = ENV.fetch("CLIENT_SECRET") # TODO: Get it from Spotify
+CLIENT_CALLBACK_URL = ENV.fetch("CLIENT_CALLBACK_URL") # TODO: Get it from Spotify
 
 # I don't think the secret should be changed...
 ENCRYPTION_SECRET = "cFJLyifeUJUBFWdHzVbykfDmPHtLKLGzViHW9aHGmyTLD8hGXC"
